873be82d77fb26f9260c9c2126c875dc2797d93a
Tjalling <tjallingts@gmail.com>
2020-01-11 17:19:41 +0100

    added toolbar too all tabs including back button on settings

--- app/src/main/java/com/example/csd_locationaware/MapsActivity.java
+++ app/src/main/java/com/example/csd_locationaware/MapsActivity.java
@@ -4,22 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import android.Manifest;
-import android.content.Context;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -28,13 +19,9 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends AppCompatActivity implements
         OnMapReadyCallback,
@@ -60,7 +47,6 @@ public class MapsActivity extends AppCompatActivity implements
         SupportMapFragment fragment = new SupportMapFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.mapView, fragment).commit();
         fragment.getMapAsync(this);
-
     }
 
 
@@ -78,11 +64,9 @@ public class MapsActivity extends AppCompatActivity implements
         mMap = googleMap;
         buildGoogleApiClient();
         mMap.setMyLocationEnabled(true);
-
-
     }
 
-    protected synchronized void buildGoogleApiClient(){
+    protected synchronized void buildGoogleApiClient() {
         boolean success = LocationUtil.checkLocationPermission(this);
 
         if (success) {
@@ -93,7 +77,7 @@ public class MapsActivity extends AppCompatActivity implements
                     .build();
 
             googleApiClient.connect();
-        }else {
+        } else {
             finish();
         }
         Log.d(TAG, "buildGoogleApiClient: " + success);
@@ -119,12 +103,32 @@ public class MapsActivity extends AppCompatActivity implements
 
     @Override
     public void onConnectionSuspended(int i) {
-
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(MapsActivity.this,SettingsActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/csd_locationaware/SettingsActivity.java
+++ app/src/main/java/com/example/csd_locationaware/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.csd_locationaware;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -11,6 +12,18 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        Toolbar toolbar = findViewById(R.id.custom_action_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Settings");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         getFragmentManager().beginTransaction().add(R.id.settings_FrameContainer, new SettingsFragment()).commit();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
